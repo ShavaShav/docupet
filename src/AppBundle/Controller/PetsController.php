ef04374057741298bd98c5ae0f996ada2ac59e22
Zach Shaver <shaverz@uwindsor.ca>
2018-11-04 21:42:21 -0500

    Add pagination to pets list route
    - Add query params to /pets: page and limit.
    
    Ex. /pets?page=2&limit4 returns 2nd page with 4 pets/page

--- src/AppBundle/Controller/PetsController.php
+++ src/AppBundle/Controller/PetsController.php
@@ -2,31 +2,47 @@
 // src/AppBundle/Controller/PetsController.php
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Pet;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use AppBundle\Entity\Pet;
 
 class PetsController extends AbstractController
 {
     /**
-     * Remders list of all pets
-     * @Route("/pets/", name="pet_list")
+     * Renders paginated list of all pets
+     * @Route("/pets", name="pet_list")
      */
-    public function listAction()
+    public function listAction(Request $request)
     {
+        $page = 1;
+        $limit = 2;
+
+        if ($request->query->has('page'))
+            $page = $request->query->get('page');
+
+        if ($request->query->has('limit'))
+            $limit = $request->query->get('limit');
+
         $pets = $this->getDoctrine()
             ->getRepository(Pet::class)
-            ->findAll();
+            ->createQueryBuilder('pet')
+            ->setFirstResult($limit * ($page - 1)) // Offset
+            ->setMaxResults($limit); // Limit
+        
+        $paginator = new Paginator($pets);
 
         return $this->render('pets/index.html.twig', [
-            'pets' => $pets,
+            'pets' => $paginator,
         ]);
     }
-
+        
     /**
      * Renders a pet's profile page
+     * @param [integer] $petId
      * @Route("/pets/{petId}", name="pet_show")
      */
     public function showAction($petId)
